070c71154f3136b9bfa987eece250efb62b311ec
ssethuraman <SivaramaKrishnan.Sethuraman1@T-Mobile.com>
2018-08-20 12:39:22 +0530

    BTFD-158 - Added BaseFragment

---
+++ app/src/main/java/com/follett/fsc/mobile/circdesk/view/base/BaseFragment.java
@@ -0,0 +1,11 @@
+/*
+ *
+ *  * Copyright (c) 2018 Follett. All rights reserved.
+ *
+ */
+
+package com.follett.fsc.mobile.circdesk.view.base;
+
+import android.support.v4.app.Fragment;
+
+public class BaseFragment extends Fragment {}
