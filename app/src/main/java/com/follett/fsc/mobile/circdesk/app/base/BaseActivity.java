5ec9178df7b47f6983cabb59ab702c6391bcda2a
Shalini Mana <mshalini@pkrmsezdc.com>
2018-09-14 17:33:24 +0530

    Call numbers & seen on or After screen implementations

--- app/src/main/java/com/follett/fsc/mobile/circdesk/app/base/BaseActivity.java
+++ app/src/main/java/com/follett/fsc/mobile/circdesk/app/base/BaseActivity.java
@@ -3,11 +3,16 @@
  */
 package com.follett.fsc.mobile.circdesk.app.base;
 
+import android.annotation.TargetApi;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
@@ -17,7 +22,13 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.follett.fsc.mobile.circdesk.R;
 import com.follett.fsc.mobile.circdesk.data.local.prefs.AppSharedPreferences;
@@ -66,8 +77,65 @@ public abstract class BaseActivity<V extends BaseViewModel> extends AppCompatAct
             }
         });
 
+        /* Please enable this for navigating to data picker screen by tapping Follet logo as of now. Need to remove this later */
+
+       /* baseBinding.logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              baseBinding = DataBindingUtil.setContentView(BaseActivity.this, R.layout.call_numbers_and_seen_on_after);
+                EditText date1 = (EditText)findViewById(R.id.enterDate);
+                date1.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        // TODO Auto-generated method stub
+                        // Initialize a new date picker dialog fragment
+                        DialogFragment dFragment = new DatePickerFragment();
+                        // Show the date picker dialog fragment
+                        dFragment.show(getFragmentManager(), "Date Picker");
+                    }
+                });
+            }
+        });*/
+
         setSupportActionBar(baseBinding.toolbar);
     }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener{
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+
+          DatePickerDialog dpd = new DatePickerDialog(getActivity());
+            // Create a TextView programmatically.
+            TextView tv = new TextView(getActivity());
+
+            // Create a TextView programmatically
+            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT, // Width of TextView
+                    RelativeLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+            tv.setLayoutParams(lp);
+            tv.setPadding(10, 10, 10, 10);
+            tv.setGravity(Gravity.LEFT);
+            tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP,14);
+            tv.setText("Seen On or After");
+            tv.setTextColor(getResources().getColor(R.color.white));
+            tv.setBackgroundColor(getResources().getColor(R.color.blueLabel));
+
+            dpd.setMessage(tv.getText()); // Uncomment this line to activate it
+
+            // Return the DatePickerDialog
+            return  dpd;
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day){
+            // Do something with the chosen date
+        }
+    }
+
+
 
     protected <T extends ViewDataBinding> T putContentView(@LayoutRes int resId) {
         return DataBindingUtil.inflate(getLayoutInflater(), resId, baseBinding.baseContainer, true);
